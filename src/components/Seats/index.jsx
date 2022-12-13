f711176d5a42677876e370879b864b001f5a3d5c
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-12-12 21:02:49 -0300

    Fixed: style of Seats component.

--- src/components/Seats/index.jsx
+++ src/components/Seats/index.jsx
@@ -45,7 +45,7 @@ const StyledSeats = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
 
   width: 100%;
   padding: 0 21px;
